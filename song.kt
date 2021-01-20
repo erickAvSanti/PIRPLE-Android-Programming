f0e57ca4291d71fc4206a9e24a7f73041b301108
Erick Avalos <ecavaloss@gmail.com>
2021-01-20 02:26:42 -0500

    Update song.kt

--- song.kt
+++ song.kt
@@ -1,11 +1,16 @@
 
 fun main() {
 
-    var artist = "The Weeknd"
-    val year_released: Int = 2019
-    var genre: String = "Synthwave, R&B/soul"
-    var duration: Float = 203f
-    val youtube_url: String = "https://www.youtube.com/watch?v=fHI8X4OXluQ"
+	/*artist variable*/
+	var artist = "The Weeknd"
+	//year released
+	val year_released: Int = 2019
+	var genre: String = "Synthwave, R&B/soul"
+	var duration: Float = 203f
+	val youtube_url: String = "https://www.youtube.com/watch?v=fHI8X4OXluQ"
+	/*
+		My printing section
+	*/
 	println("artist = "+artist)
 	println("year_released = "+year_released)
 	println("genre = "+genre)
