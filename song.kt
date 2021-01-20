b96da4a5d27415860adca040214aa04f18066fd2
Erick Avalos <ecavaloss@gmail.com>
2021-01-20 02:24:45 -0500

    Create song.kt

---
+++ song.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+
+    var artist = "The Weeknd"
+    val year_released: Int = 2019
+    var genre: String = "Synthwave, R&B/soul"
+    var duration: Float = 203f
+    val youtube_url: String = "https://www.youtube.com/watch?v=fHI8X4OXluQ"
+	println("artist = "+artist)
+	println("year_released = "+year_released)
+	println("genre = "+genre)
+	println("duration = "+duration+" seconds")
+	println("youtube_url = "+youtube_url)
+}
